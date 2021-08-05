34fe9df177de9b1013579a3ae4199ff6997d4886
urgentiser <31138562+urgentiser@users.noreply.github.com>
2021-08-05 10:01:08 +0200

    Add files via upload

---
+++ ApiClient.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace NyikoConsoleApp
+{
+
+    class ApiClient
+    {
+        private readonly string _baseUri;
+        private readonly string _dataType;
+        private readonly bool _enabled;
+
+        public ApiClient(string baseUri, string dataType, bool enabled)
+        {
+            this._baseUri = baseUri;
+            this._dataType = dataType;
+            this._enabled = enabled;
+        }
+
+        public async Task<R_type> DeserializeJSON<R_type>(string resource)
+        {
+            using (var _httpClient = new HttpClient())
+            {
+                try
+                {
+                    _httpClient.DefaultRequestHeaders.Clear();
+
+                    HttpResponseMessage httpResponseMessage = await _httpClient.GetAsync(_baseUri + resource);
+                    httpResponseMessage.EnsureSuccessStatusCode();
+
+                    if (httpResponseMessage.IsSuccessStatusCode && _dataType == "JSON" && _enabled == true)
+                    {
+
+                        var response = httpResponseMessage.Content.ReadAsStringAsync().Result;
+
+                        return JsonConvert.DeserializeObject<R_type>(response);
+                    }
+
+                    else
+                    {
+                        throw new Exception("Connection failed");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+
+    }
+    class ApiClientXML
+    {
+        private readonly string _baseUri;
+        private readonly string _dataType;
+        private readonly bool _enabled;
+
+        public ApiClientXML(string baseUri, string dataType, bool enabled)
+        {
+            this._baseUri = baseUri;
+            this._dataType = dataType;
+            this._enabled = enabled;
+        }
+        public async Task<T> DeserializeXml<T>(string resource) where T : class
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof(T));
+                T response = null;
+                var _httpClient = new HttpClient();
+                using (TextReader reader = new StringReader(resource))
+                {
+                    _httpClient.DefaultRequestHeaders.Clear();
+                    HttpResponseMessage httpResponseMessage = await _httpClient.GetAsync(_baseUri + resource);
+                    httpResponseMessage.EnsureSuccessStatusCode();
+                    if (httpResponseMessage.IsSuccessStatusCode && _dataType == "XML" && _enabled == true)
+                    {
+                        var res = httpResponseMessage.Content.ReadAsStringAsync().Result;
+
+                        response = (T)serializer.Deserialize(reader);
+                    }
+                    else
+                    {
+                        throw new Exception("Connection failed");
+                    }
+
+                }
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using NyikoConsoleApp.Model;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace NyikoConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           
+
+
+            var fileData = await FileHelper.Read(@"C:\Users\User\Downloads\BitVenture_-_Coding_Project\bonus_endpoints.json");
+
+            var _fileContent = JObject.Parse(fileData);
+
+
+            var services = _fileContent.ToObject<Root>(); 
+
+
+            foreach(var service in services.services)
+            {
+                ApiClient clientJSON = new ApiClient(service.baseURL, service.datatype, service.enabled);
+                ApiClientXML clientXML = new ApiClientXML(service.baseURL, service.datatype, service.enabled);
+                foreach (var _endpoint in service.endpoints)
+                {
+                   
+                    if(service.datatype=="JSON")
+                    {
+                        var resp = await clientJSON.DeserializeJSON<dynamic>(_endpoint.resource);
+                        Console.WriteLine("Info: {0}", resp);
+
+                        Console.WriteLine();
+                    }
+                    else if (service.datatype == "XML")
+                    {
+                        var response = await clientXML.DeserializeXml<dynamic>(_endpoint.resource);
+                        Console.WriteLine("Info: {0}", response);
+
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine(ErrorEventArgs.Empty);
+                    }
+
+                }
+            }
+           
+
+        }
+    }
+
+    class FileHelper 
+    {
+        public static async Task<string> Read(string fullPath)
+        {
+            try
+            {
+                var fileContent = await File.ReadAllTextAsync(fullPath);
+                return fileContent;
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
